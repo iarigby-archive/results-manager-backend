b4de35bc70adc2ab3def2e58ad3a76e51dd76940
iarigby <marvinzem@gmail.com>
2020-01-05 23:29:32 +0400

    I actually tested the refactor, god bless

---
+++ test/api.test.js
@@ -0,0 +1,19 @@
+const request = require('supertest')
+const app = require('../api/app')
+const expect = require('chai').expect
+
+describe('basic functionality test', () => {
+    it('shoud start up', async() => {
+        const res = await request(app)
+            .get('/')
+        expect(res.statusCode).equal(200)
+        expect(res.body.status).equal('OK')
+    })
+
+    it('shoud route the miterms', async() => {
+        const res = await request(app)
+            .get('/midterms/4')
+        expect(res.statusCode).equal(200)
+        expect(res.body.midtermId).equal('4')
+    })
+})
